37d2a718105d32b437ea18672f1de1d82e514907
Sandesh GC <gcsandesh01@gmail.com>
2022-10-15 23:40:07 +0545

    completed the menu button for mobile

---
+++ components/NavBar/NavLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../../styles/NavBar.module.css";
+
+export default function NavLinks(props) {
+  return (
+    <ul className={styles.navLinks}>
+      <li onClick={props.toggleMenu}>
+        <Link href={"/"}>Home</Link>
+      </li>
+      <li onClick={props.toggleMenu}>
+        <Link href={"/shop"}>Shop</Link>
+      </li>
+      <li onClick={props.toggleMenu}>
+        <Link href={"/about"}>About</Link>
+      </li>
+      <li onClick={props.toggleMenu}>
+        <Link href={"/contact"}>Contact</Link>
+      </li>
+    </ul>
+  );
+}
