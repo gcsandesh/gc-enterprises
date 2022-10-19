1f97ae984605fbd2aa9771fdf06d8c413fe8cdee
Sandesh GC <gcsandesh01@gmail.com>
2022-10-19 23:11:16 +0545

    fixed deployment errors

--- pages/shop/men.js
+++ pages/shop/men.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Men() {
   return (
-    <div>Men's Products</div>
+    <div>Mens Products</div>
   )
 }
--- pages/shop/women.js
+++ pages/shop/women.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Women() {
   return (
     <div>
-      Women's Products
+      Womens Products
     </div>
   );
 }
